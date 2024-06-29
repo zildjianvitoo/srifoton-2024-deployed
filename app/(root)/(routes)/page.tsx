2ef359cb1013dc83cbca94fe218d320563667091
anotherlilentity <afifverdian@gmail.com>
2024-06-29 17:21:31 +0700

    merge with branch dev

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,13 @@
 import Countdown from "@/components/Root/Home/Countdown";
 import Hero from "@/components/Root/Home/Hero";
 import Nav from "@/components/Root/Home/Nav";
+import Footer from "@/components/Root/Home/Footer";
 
 export default function Home() {
   return (
     <main className="bg-background">
       <Hero />
+      <Footer />
     </main>
   );
 }
--- components/Root/Home/Footer.tsx
+++ components/Root/Home/Footer.tsx
@@ -1,11 +1,32 @@
-import { Link } from "lucide-react";
+import * as React from 'react';
+import { X, Facebook, Instagram, Tiktok, Whatsapp, Email } from '../../../lib/icon';
+import Link from 'next/link';
+import LocalFont from "next/font/local";
+
+const ponnala = LocalFont({
+  src: "../../../public/fonts/Ponnala Regular.woff2",
+  variable: "--font-ponnala",
+});
+
+const social = [
+  {linkTo: 'x', address: '#', 'icon': X},
+  {linkTo: 'facebook', address: '#', 'icon': Facebook},
+  {linkTo: 'instagram', address: '#', 'icon': Instagram},
+  {linkTo: 'tiktok', address: '#', 'icon': Tiktok},
+];
+
+const contact = [
+  {linkTo: 'whatsapp', address: '#', 'icon': Whatsapp},
+  {linkTo: 'email', address: '#', 'icon': Email}
+];
 
 export default function Footer() {
+
   return(
     <>
-      <footer className="w-screen">
+      <footer className="w-screen space-y-57">
 
-        <div className="w-screen space-y-57">
+        <div className="">
           <span className="w-224 h-198 bg-purple-700">
             <img src="#" alt="Logo" />
           </span>
@@ -13,27 +34,17 @@ export default function Footer() {
 
         <hr />
 
-        <div className="w-screen">
-          <div className="space-x-116">
-            <span className="w-224 h-198 bg-purple-700">
-              <img src="#" alt="Logo" />
-            </span>
-            <p>SRIFOTON 2024</p>
+        <div className="inline-flex w-screen justify-between">
+          <div className={`${ponnala.variable} text-white`} style={{fontSize: '13px'}}>
+            <p>© SRIFOTON 2024</p>
           </div>
-          <div className="space-x-26">
-            <ul>
-              <li>
-                <Link><i></i></Link>
-              </li>
-              <li>
-                <Link><i></i></Link>
-              </li>
-              <li>
-                <Link><i></i></Link>
-              </li>
-              <li>
-                <Link><i></i></Link>
-              </li>
+          <div className="">
+            <ul className="list-none inline-flex">
+              {social.map((link) => (
+                <li key={link.linkTo} className='m-13 p-0'>
+                  <Link href={link.address}><link.icon /></Link>
+                </li>
+              ))}
             </ul>
           </div>
         </div>

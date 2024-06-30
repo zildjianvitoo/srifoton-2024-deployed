70f9dd25e9395c459fcfa2fb4624d0424a99100c
mhasaero <eroouji7314@gmail.com>
2024-06-30 13:35:08 +0700

    feat: mascot

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,6 +1,7 @@
 import Countdown from "@/components/Root/Home/Countdown";
 import Hero from "@/components/Root/Home/Hero";
 import Gallery from "@/components/Root/Home/Gallery";
+import Mascot from "@/components/Root/Home/Mascot";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Countdown />
       <Gallery />
+      <Mascot />
     </main>
   );
 }
---
+++ components/Root/Home/Mascot.tsx
@@ -0,0 +1,10 @@
+export default function Mascot() {
+  return (
+    <section id="mascot" className="mt-40 px-8 md:px-12 lg:px-20 xl:px-[7rem]">
+      <h3 className="font-inter text-lg text-primary md:text-3xl lg:text-4xl xl:text-5xl">
+        NAMA MASKOT
+      </h3>
+      <div className="mx-auto mt-20 h-[10rem] w-[40%] bg-primary md:h-[20rem] lg:h-[25rem] xl:h-[30rem]"></div>
+    </section>
+  );
+}

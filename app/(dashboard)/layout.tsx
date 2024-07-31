ca159523bdfd32ef2a454fbd7f16ec047d747868
mhasaero <eroouji7314@gmail.com>
2024-07-31 20:45:46 +0700

    feat(dashboard): add nav-sided
    belum selesai bagian mobile

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,17 @@
 import React, { ReactNode } from "react";
+import NavSided from "@/components/Root/Dashboard/NavSided";
 
 type Props = {
   children: ReactNode;
 };
 
 export default function layout({ children }: Props) {
-  return <main>{children}</main>;
+  return (
+    <div className="bg-background">
+      <NavSided />
+      <main className="text- mx-[7.3vw] mt-12 flex flex-col gap-24 lg:mt-[75px] lg:gap-[200px]">
+        {children}
+      </main>
+    </div>
+  );
 }
--- lib/link.tsx
+++ lib/link.tsx
@@ -1,27 +1,33 @@
-import * as icon from '@/lib/icon';
+import * as icon from "@/lib/icon";
+import { Contact, CircleUser, CalendarRange } from "lucide-react";
 
 const socials = [
-  {linkTo: 'X', address: '#', 'icon': icon.X},
-  {linkTo: 'Facebook', address: '#', 'icon': icon.Facebook},
-  {linkTo: 'Instagram', address: '#', 'icon': icon.Instagram},
-  {linkTo: 'Tiktok', address: '#', 'icon': icon.Tiktok},
+  { linkTo: "X", address: "#", icon: icon.X },
+  { linkTo: "Facebook", address: "#", icon: icon.Facebook },
+  { linkTo: "Instagram", address: "#", icon: icon.Instagram },
+  { linkTo: "Tiktok", address: "#", icon: icon.Tiktok },
 ];
 
 const contacts = [
-  {linkTo: 'Whatsapp', contact: [
-    '+62 xxxxxxxx',
-    '+62 xxxxxxxx'
-  ], 'icon': icon.Whatsapp},
-  {linkTo: 'Email', contact: [
-    'srifoton.unsri@gmail.com'
-  ], 'icon': icon.Email}
+  {
+    linkTo: "Whatsapp",
+    contact: ["+62 xxxxxxxx", "+62 xxxxxxxx"],
+    icon: icon.Whatsapp,
+  },
+  { linkTo: "Email", contact: ["srifoton.unsri@gmail.com"], icon: icon.Email },
 ];
 
 const routes = [
-  {linkTo: 'Home', address: '/'},
-  {linkTo: 'Competition', address: '/competition'},
-  {linkTo: 'Workshop', address: '/workshop'},
-  {linkTo: 'Talkshow', address: '/talkshow'}
-]
+  { linkTo: "Home", address: "/" },
+  { linkTo: "Competition", address: "/competition" },
+  { linkTo: "Workshop", address: "/workshop" },
+  { linkTo: "Talkshow", address: "/talkshow" },
+];
 
-export {socials, contacts, routes};
+const dashboardRoutes = [
+  { linkTo: "Account Data", address: "/account-data", icon: <Contact /> },
+  { linkTo: "Personal Data", address: "/personal-data", icon: <CircleUser /> },
+  { linkTo: "Activities", address: "/activities", icon: <CalendarRange /> },
+];
+
+export { socials, contacts, routes, dashboardRoutes };

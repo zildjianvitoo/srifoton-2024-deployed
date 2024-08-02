240884717d2261aed529f012e3e0d840a45a8958
mhasaero <eroouji7314@gmail.com>
2024-08-02 22:03:12 +0700

    feat(dashboard): add section account-data

---
+++ components/Root/CardCrook.dasboard.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+type Props = { className?: string; children: ReactNode };
+
+export default function CardCrook({ className, children, ...props }: Props) {
+  return (
+    <div
+      className={cn(
+        "mb-30 relative w-2/3 font-monument text-text-200 md:w-3/5",
+        className,
+      )}
+    >
+      <div className="absolute z-0 mx-auto h-full w-full rotate-[355deg] bg-[#868365]"></div>
+      <div
+        className={
+          "relative z-10 mx-auto flex h-full w-full flex-col bg-primary-100 p-8 lg:py-14"
+        }
+        {...props}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}

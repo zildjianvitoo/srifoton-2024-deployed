f233d5451fad5e337153a0cea6157957af928a0a
mhasaero <eroouji7314@gmail.com>
2024-08-07 20:07:27 +0700

    feat(auth): add reset password

---
+++ components/Auth/FormResetPassword.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { PasswordField } from "../Root/Dashboard/PasswordField";
+import "@/lib/utils/zodCustomError";
+
+type dataProps = {
+  name?: string;
+  email?: string;
+  password?: string;
+};
+
+async function addData({ name, email, password }: dataProps) {
+  console.log(name, email, password);
+}
+
+const formSchema = z
+  .object({
+    password: z
+      .string({
+        required_error: "Password can not be empty.",
+      })
+      .regex(/^.{8,20}$/, {
+        message: "Minimum 8 and maximum 20 characters.",
+      })
+      .regex(/(?=.*[A-Z])/, {
+        message: "At least one uppercase character.",
+      })
+      .regex(/(?=.*[a-z])/, {
+        message: "At least one lowercase character.",
+      })
+      .regex(/(?=.*\d)/, {
+        message: "At least one digit.",
+      }),
+    password1: z
+      .string({
+        required_error: "Password can not be empty.",
+      })
+      .regex(/^.{8,20}$/, {
+        message: "Minimum 8 and maximum 20 characters.",
+      })
+      .regex(/(?=.*[A-Z])/, {
+        message: "At least one uppercase character.",
+      })
+      .regex(/(?=.*[a-z])/, {
+        message: "At least one lowercase character.",
+      })
+      .regex(/(?=.*\d)/, {
+        message: "At least one digit.",
+      }),
+  })
+  .refine(({ password, password1 }) => password === password1, {
+    path: ["password1"],
+    message: "Password didn't match.",
+  });
+
+export default function FormResetPassword() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      password: "",
+      password1: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    addData(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-10 space-y-4 text-black md:px-8"
+      >
+        <PasswordField
+          title="New Password"
+          name="password"
+          placeholder="Masukkan kata sandi baru"
+        />
+        <PasswordField
+          title="Confirm Password"
+          name="password1"
+          placeholder="Konfirmasi kata sandi"
+        />
+        <Button
+          type="submit"
+          className="w-full bg-background text-sm text-white"
+        >
+          Reset Password
+        </Button>
+      </form>
+    </Form>
+  );
+}

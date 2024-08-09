ba77a113ff88cedba7f9474c60ae21d6a2090616
M. Fazri Nizar <anon6372098.id@gmail.com>
2024-08-09 21:51:14 +0700

    Fix(root-layout): hide overflow

--- app/(root)/(routes)/competition/(info)/competitive-programming/daftar/page.tsx
+++ app/(root)/(routes)/competition/(info)/competitive-programming/daftar/page.tsx
@@ -1,11 +1,17 @@
+"use client"
+
 import CompetitionRegistration from "@/components/Root/Competition/CompetitionRegistration";
 import { auth } from "@/lib/firebase";
+import { use, useEffect } from "react";
 import { toast } from "sonner";
 
 export default function CPRegistration() {
-  if (auth.currentUser === null) {
-    toast.error("Anda harus login terlebih dahulu untuk bisa mendaftar!");
-  }
+
+  useEffect(() => {
+    // if (auth.currentUser === null) {
+      toast.error("Anda harus login terlebih dahulu untuk bisa mendaftar!");
+  // }
+  }, []);
 
   return (
     <>
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function MainLayout({ children }: Props) {
   return (
-    <div className="bg-background">
+    <div className="bg-background overflow-x-hidden">
       <Nav />
       <main className="mt-12 flex flex-col gap-[120px] px-[6.2917vw] md:gap-[150px] lg:mt-[75px] lg:gap-[200px]">
         {children}

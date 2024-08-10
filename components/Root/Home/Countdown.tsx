614fa594e469a1fa2bd21b07cbddc52f71b85244
mhasaero <eroouji7314@gmail.com>
2024-08-10 22:02:12 +0700

    feat(countdown): extend countdown to 09/12/2024

--- components/Root/Home/Countdown.tsx
+++ components/Root/Home/Countdown.tsx
@@ -15,7 +15,7 @@ function DoubleDot() {
 
 export default function Countdown() {
   let { days, hours, minutes, seconds } = useCountdown({
-    targetTime: "08/10/2024 23:59:59",
+    targetTime: "09/12/2024 23:59:59",
   });
 
   return (

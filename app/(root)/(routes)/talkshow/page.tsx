84951da850fdb535936a7ae558bc8d55c69add64
zildjianvitoo <zildjianvito07@gmail.com>
2024-08-10 14:03:52 +0700

    fix: talkshow page

--- app/(root)/(routes)/talkshow/page.tsx
+++ app/(root)/(routes)/talkshow/page.tsx
@@ -5,21 +5,6 @@ import IDcard from "@/components/ui/IDcard";
 import { Flashcard, Flashcards, Benefits } from "@/components/Root/Flashcards";
 import { QNA, SpeechBubble } from "@/components/Root/QNA";
 import { Timeline, TimelineCard } from "@/components/Root/Timeline";
-import React, { Suspense } from "react";
-import SectionHeader from "@/components/Root/SectionHeader";
-import {
-  Accordion,
-  AccordionItem,
-  AccordionTrigger,
-  AccordionContent,
-} from "@radix-ui/react-accordion";
-import { useRef } from "react";
-import {
-  Accordion,
-  AccordionItem,
-  AccordionTrigger,
-  AccordionContent,
-} from "@radix-ui/react-accordion";
 import { useRef } from "react";
 
 type Props = {};
@@ -33,26 +18,14 @@ export default function Talkshow({}: Props) {
         id="hero-talkshow"
         header="talkshow"
         subheader="srifoton 2024"
-        src="/img/gallery1.png"
+        src="/img/talkshow-logo-2.png"
         onClick={() =>
           workshopRef.current?.scrollIntoView({ behavior: "smooth" })
         }
       >
-        “Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa
-        mi. Aliquam in hendrerit urna.”
+        “Effective Ways to Build and Maintain a Personal Brand in the Digital
+        Era”
       </OverviewHero>
-      <IDcard
-        title="our speaker"
-        name="nama speaker"
-        src1="/img/gallery1.png"
-        src2="/img/gallery1.png"
-      >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.
-        Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla,
-        mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis
-        tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non
-        suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus
-        ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales
       <IDcard
         title="our speaker"
         name="nama speaker"
@@ -67,14 +40,6 @@ export default function Talkshow({}: Props) {
         ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales
       </IDcard>
       <div ref={workshopRef}>
-        <Flashcards id="talkshow-option" title="talkshow options">
-          <Flashcard
-            title="talkshow ticket"
-            previewTitle="offline ticket"
-            href="talkshow/daftar"
-            price="40.000"
-            per="person"
-          >
         <Flashcards id="talkshow-option" title="talkshow options">
           <Flashcard
             title="talkshow ticket"
@@ -106,17 +71,6 @@ export default function Talkshow({}: Props) {
             price="40.000"
             per="person"
           >
-          <Flashcard
-            title={
-              <>
-                workshop online + <br /> talkshow
-              </>
-            }
-            previewTitle="combo package 1"
-            href="talkshow/daftar"
-            price="40.000"
-            per="person"
-          >
             <p>
               SRIFOTON menawarkan workshop &quot;Kickstart Your Web Development
               Journey&quot; dan talkshow &quot;Effective Ways to Build and
@@ -131,17 +85,6 @@ export default function Talkshow({}: Props) {
               <p>Snack + Lunch</p>
             </Benefits>
           </Flashcard>
-          <Flashcard
-            title={
-              <>
-                workshop offline + <br /> talkshow
-              </>
-            }
-            previewTitle="combo package 2"
-            href="talkshow/daftar"
-            price="40.000"
-            per="person"
-          >
           <Flashcard
             title={
               <>
@@ -194,34 +137,10 @@ export default function Talkshow({}: Props) {
         <TimelineCard span="23 Sept - 20 Augs">
           Open Registration Competition
         </TimelineCard>
-        <TimelineCard span="23 Sept - 20 Augs">
-          Open Registration Competition
-        </TimelineCard>
-        <TimelineCard span="23 Sept - 20 Augs">
-          Open Registration Competition
-        </TimelineCard>
-        <TimelineCard span="23 Sept - 20 Augs">
-          Open Registration Competition
-        </TimelineCard>
-        <TimelineCard span="23 Sept - 20 Augs">
-          Open Registration Competition
-        </TimelineCard>
-        <TimelineCard span="23 Sept - 20 Augs">
-          Open Registration Competition
-        </TimelineCard>
-        <TimelineCard span="23 Sept - 20 Augs">
-          Open Registration Competition
-        </TimelineCard>
-        <TimelineCard span="23 Sept - 20 Augs">
-          Open Registration Competition
-        </TimelineCard>
-        <TimelineCard span="23 Sept - 20 Augs">
-          Open Registration Competition
-        </TimelineCard>
       </Timeline>
       <QNA id="workshop-qna" title="workshop qna">
         <SpeechBubble id="speech1" question="Bagaimana cara mendaftar?">
-          Silahkan klik tombol &quot;Register&quot; pada acara yang ingin Anda
+          Silakan klik tombol &quot;Register&quot; pada acara yang ingin Anda
           ikuti, kemudian ikuti langkah-langkah untuk menyelesaikan proses
           pendaftaran.
         </SpeechBubble>
@@ -232,28 +151,14 @@ export default function Talkshow({}: Props) {
           id="speech3"
           question="Apakah talkshow akan direkam selama acara berlangsung dan dibagikan kepada peserta?"
         >
-        <SpeechBubble
-          id="speech3"
-          question="Apakah talkshow akan direkam selama acara berlangsung dan dibagikan kepada peserta?"
-        >
           Ya, rekaman akan dibagikan setelah acara selesai.
         </SpeechBubble>
         <SpeechBubble
           id="speech4"
           question="Apa dress code untuk menghadiri acara?"
         >
-        <SpeechBubble
-          id="speech4"
-          question="Apa dress code untuk menghadiri acara?"
-        >
           Dress code adalah bebas tetapi tetap sopan.
         </SpeechBubble>
-        <SpeechBubble
-          id="speech5"
-          question="Fasilitas apa saja yang disediakan selama talkshow berlangsung?"
-        >
-          Akan ada camilan dan makan siang. Selain itu, HMIF akan membuka stan
-          Official Merchandise SRIFOTON 2024.
         <SpeechBubble
           id="speech5"
           question="Fasilitas apa saja yang disediakan selama talkshow berlangsung?"

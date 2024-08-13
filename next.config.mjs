9945cfe169c2968838d266e96c2e94b4a616fa9b
M. Fazri Nizar <anon6372098.id@gmail.com>
2024-08-13 12:44:10 +0700

    Feat(auth-domain): use srifoton.hmifunsri.com as rewrites of default firebase domain for reset/verify email

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 // next.config.mjs
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/__/auth/:path(.*)',
+                destination: 'https://srifoton-2024.firebaseapp.com/__/auth/:path*',
+            }
+        ]
+    },
+};
 
 export default nextConfig;
